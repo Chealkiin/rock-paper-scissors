e4a4e69b527eee6287f0cb3d4730238f4edf46d6
Chealkiin <chealkiin@gmail.com>
2022-10-06 22:15:11 +1100

    Added animation, GUI functionality. There is a bug when displaying image for computer's choice here

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,53 +1,153 @@
 /* rock paper scissors coded by Chealkiin */
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let playerChoice;
-  let computerChoice;
+const playerRock = document.getElementById('rock');
+const playerPaper = document.getElementById('paper');
+const playerScissors = document.getElementById('scissors');
+const animation = document.querySelector('#animated');
+const playerScoreDisplay = document.querySelector('#playerScore');
+const compScoreDisplay = document.querySelector('#computerScore');
+const resultsDisplay = document.querySelector('#result');
+const resultsContainer = document.querySelector('#resultsContainer');
+let playerScore = 0;
+let computerScore = 0;
+let computerChoice;
+playerRock.addEventListener('click', playRock);
+playerPaper.addEventListener('click', playPaper);
+playerScissors.addEventListener('click', playScissors);
 
-  function generateSelection() {
-    return generatedNumber = Math.floor(Math.random() * 3);
-  }
-  
-  function computerSelection(n) {
-    if (n === 0) {
-      n = 'rock';
-    } else if (n === 1) {
-      n = 'paper';
-    } else {
-      n = 'scissors';
-    }
-    return n;
-  }
-  
-  function playRound(userChoice, computerChoice) {
-    computerChoice = computerSelection(generateSelection());
-    if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || (userChoice === 'scissors' && computerChoice === 'paper')) {
-      return playerScore ++,
-      alert(`Congratulations, you win! ` + userChoice + ` beats ` + computerChoice + `.\n Your score: ` + playerScore + ` Computer's score: ` + computerScore);
-    } else if (userChoice === computerChoice) {
-      return alert(`It is a tie! You both picked ` + userChoice + `.\n Your score: ` + playerScore + ` Computer's score: ` + computerScore);
-    } else  if ((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || (computerChoice === 'scissors' && userChoice === 'paper')) {
-      return computerScore++,
-      alert(`Oh man, you lose! ` + userChoice + ` does not beat ` + computerChoice + `.\n Your score: ` + playerScore + ` Computer's score: ` + computerScore);
-    } else {
-      return alert(`You must pick 'Rock', 'Paper' or 'Scissors'!!`);
-    }
-  }
-
-  while ((playerScore < 5) && (computerScore < 5)) {
-    playerChoice = prompt('Choose: Rock, Paper or Scissors?').toLowerCase();
-    playRound(playerChoice, computerChoice);
-  }
-
-  if (playerScore === 5 || computerScore === 5) {
-    if (confirm('Do you want to play again?')) {
-      game();
-    } else {
-      alert('Nice playing, see you next time!');
-    }
-  }
-
+function generateNumber() {
+  return Math.floor(Math.random() * 3);
 }
 
-game();
+function computerSelection(n) {
+  if (n === 0) {
+    n = 'rock';
+  } else if (n === 1) {
+    n = 'paper';
+  } else if (n === 2) {
+    n = 'scissors';
+  }
+  return n;
+}
+
+function animateComputer() {
+  animation.src = 'img/rock.png';
+  animation.classList.toggle('animate');
+  setTimeout(function() {
+    animation.classList.toggle('animate');
+  }, 1600);
+  if (computerChoice === 'rock') {
+    animation.addEventListener('animationend', updateRock);
+  } else if (computerChoice === 'paper') {
+    animation.addEventListener('animationend', updatePaper);
+  } else if (computerChoice === 'scissors') {
+    animation.addEventListener('animationend', updateScissors);
+  }
+}
+
+function updatePaper() {
+  animation.src = 'img/paper.png';
+}
+function updateRock() {
+  animation.src = 'img/rock.png';
+}
+function updateScissors() {
+  animation.src = 'img/scissors.png';
+}
+
+function playRock() {
+  computerChoice = computerSelection(generateNumber());
+  if (computerChoice === 'scissors') {
+    return playerScore++,
+    setTimeout(function() {
+      resultsContainer.style.backgroundColor = '#9aa5bd';
+      playerScoreDisplay.textContent = `Player Score: ${playerScore}` ,
+      compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+      resultsDisplay.textContent = `WIN!`  }, 1600),
+    animateComputer();
+  } else if (computerChoice === 'rock') {
+    setTimeout(function() {
+      resultsContainer.style.backgroundColor = '#9aa5bd';
+      playerScoreDisplay.textContent = `Player Score: ${playerScore}`,
+      compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+      resultsDisplay.textContent = `DRAW!`
+    }, 1600),
+    animateComputer();
+  } else  if (computerChoice === 'paper') {
+    computerScore++,
+    setTimeout(function() {
+      resultsContainer.style.backgroundColor = '#9aa5bd';
+      playerScoreDisplay.textContent = `Player Score: ${playerScore}`,
+      compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+      resultsDisplay.textContent = `LOSE!`
+    }, 1600),
+    animateComputer();
+  } else {
+    console.log(`I have no idea what you've done here...`);
+  }
+}
+
+function playPaper() {
+  computerChoice = computerSelection(generateNumber());
+  if (computerChoice === 'rock') {
+    return playerScore++,
+      setTimeout(function() {
+        resultsContainer.style.backgroundColor = '#9aa5bd';
+        playerScoreDisplay.textContent = `Player Score: ${playerScore}` ,
+        compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+        resultsDisplay.textContent = `WIN!`
+      }, 1600),
+      animateComputer();
+  } else if (computerChoice === 'paper') {
+      setTimeout(function() {
+        resultsContainer.style.backgroundColor = '#9aa5bd';
+        playerScoreDisplay.textContent = `Player Score: ${playerScore}`,
+        compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+        resultsDisplay.textContent = `DRAW!`
+      }, 1600),
+      animateComputer();
+  } else  if (computerChoice === 'scissors') {
+    computerScore++,
+    setTimeout(function() {
+      resultsContainer.style.backgroundColor = '#9aa5bd';
+      playerScoreDisplay.textContent = `Player Score: ${playerScore}`,
+      compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+      resultsDisplay.textContent = `LOSE!`
+    }, 1600),
+    animateComputer();
+  } else {
+    console.log(`I have no idea what you've done here...`);
+  }
+}
+
+function playScissors() {
+  computerChoice = computerSelection(generateNumber());
+  if (computerChoice === 'paper') {
+    return playerScore++,
+    setTimeout(function() {
+      resultsContainer.style.backgroundColor = '#9aa5bd';
+      playerScoreDisplay.textContent = `Player Score: ${playerScore}` ,
+      compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+      resultsDisplay.textContent = `WIN!`
+    }, 1600),
+    animateComputer();
+  } else if (computerChoice === 'scissors') {
+      setTimeout(function() {
+        resultsContainer.style.backgroundColor = '#9aa5bd';
+        playerScoreDisplay.textContent = `Player Score: ${playerScore}`,
+        compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+        resultsDisplay.textContent = `DRAW!`
+      }, 1600),
+      animateComputer();
+  } else  if (computerChoice === 'rock') {
+    computerScore++,
+    setTimeout(function() {
+      resultsContainer.style.backgroundColor = '#9aa5bd';
+      playerScoreDisplay.textContent = `Player Score: ${playerScore}`,
+      compScoreDisplay.textContent = `Player Score: ${computerScore}`,
+      resultsDisplay.textContent = `LOSE!`
+    }, 1600),
+    animateComputer();
+  } else {
+    console.log(`I have no idea what you've done here...`);
+  }
+}
